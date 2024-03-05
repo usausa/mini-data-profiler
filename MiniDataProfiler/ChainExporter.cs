db67fd613cf9300dd9aba5263f24e1a8693ba90c
usausa <machi.pon@gmail.com>
2024-03-05 21:52:53 +0900

    Add chain exporter

---
+++ MiniDataProfiler/ChainExporter.cs
@@ -0,0 +1,37 @@
+namespace MiniDataProfiler;
+
+using System.Data.Common;
+
+public sealed class ChainExporter : IProfileExporter
+{
+    private readonly IProfileExporter[] exporters;
+
+    public ChainExporter(params IProfileExporter[] exporters)
+    {
+        this.exporters = exporters;
+    }
+
+    public void OnExecuteStart(DbCommand command)
+    {
+        foreach (var exporter in exporters)
+        {
+            exporter.OnExecuteStart(command);
+        }
+    }
+
+    public void OnExecuteFinally(DbCommand command, long elapsed)
+    {
+        foreach (var exporter in exporters)
+        {
+            exporter.OnExecuteFinally(command, elapsed);
+        }
+    }
+
+    public void OnError(DbCommand command, Exception ex)
+    {
+        foreach (var exporter in exporters)
+        {
+            exporter.OnError(command, ex);
+        }
+    }
+}
