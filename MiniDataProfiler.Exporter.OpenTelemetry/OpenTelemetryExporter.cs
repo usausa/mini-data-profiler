f0b63b455fdcf4295c9722f4b21040a962b7c140
usausa <machi.pon@gmail.com>
2024-03-18 16:11:45 +0900

    Update exporter

--- MiniDataProfiler.Exporter.OpenTelemetry/OpenTelemetryExporter.cs
+++ MiniDataProfiler.Exporter.OpenTelemetry/OpenTelemetryExporter.cs
@@ -15,11 +15,20 @@ public sealed class OpenTelemetryExporterOption
 
 public sealed class OpenTelemetryExporter : IProfileExporter, IDisposable
 {
+#pragma warning disable SA1401 // Fields should be private
+    private sealed class AsyncLocalData
+    {
+        public Activity? Activity;
+
+        public bool HasError;
+    }
+#pragma warning restore SA1401 // Fields should be private
+
     private readonly OpenTelemetryExporterOption option;
 
     private readonly ActivitySource activitySource;
 
-    private static readonly AsyncLocal<Activity?> ActivityLocal = new();
+    private static readonly AsyncLocal<AsyncLocalData?> LocalData = new();
 
     public OpenTelemetryExporter(OpenTelemetryExporterOption option)
     {
@@ -32,35 +41,71 @@ public sealed class OpenTelemetryExporter : IProfileExporter, IDisposable
         activitySource.Dispose();
     }
 
+    private static AsyncLocalData GetAsyncLocalData()
+    {
+        var data = LocalData.Value;
+        if (data is null)
+        {
+            data = new AsyncLocalData();
+            LocalData.Value = data;
+        }
+
+        return data;
+    }
+
     public void OnExecuteStart(EventType eventType, DbCommand command)
     {
-        var activity = activitySource.StartActivity(eventType.AsString());
-        ActivityLocal.Value = activity;
+        var activity = activitySource.CreateActivity(eventType.AsString(), ActivityKind.Client);
+
+        var data = GetAsyncLocalData();
+        data.Activity = activity;
+        data.HasError = false;
+
+        if (activity is null)
+        {
+            return;
+        }
 
         if (option.UseSqlTag)
         {
-            activity?.SetTag("profiler.sql", command.CommandText);
+            activity.SetTag("data.sql", command.CommandText);
         }
 
         if (option.UseParameterTag)
         {
-            activity?.SetTag("profiler.parameter", MakeParameterText(command));
+            activity.SetTag("data.parameter", MakeParameterText(command));
         }
+
+        activity.Start();
     }
 
     public void OnExecuteFinally(EventType eventType, DbCommand command, TimeSpan elapsed)
     {
-        ActivityLocal.Value?.Dispose();
-        ActivityLocal.Value = null;
+        var data = GetAsyncLocalData();
+        var activity = data.Activity;
+        if (activity is not null)
+        {
+            activity.SetTag("otel.status_code", data.HasError ? "ERROR" : "OK");
+            activity.Dispose();
+            data.Activity = null;
+        }
+        data.HasError = false;
     }
 
     public void OnError(EventType eventType, DbCommand command, Exception ex)
     {
-        var activity = ActivityLocal.Value;
+        var data = GetAsyncLocalData();
+        data.HasError = true;
+
+        var activity = data.Activity;
+        if (activity is null)
+        {
+            return;
+        }
 
         if (option.UseParameterTag)
         {
-            activity?.SetTag("profiler.exception", ex);
+            activity.SetTag("data.exception", ex);
         }
     }
 
